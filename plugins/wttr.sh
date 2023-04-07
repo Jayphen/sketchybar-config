9f2aea4acb41f35f9ac424fa557bd9047275b19c
Jayphen <software@jayphen.com>
2023-04-07 21:03:44 +0200

    Init

---
+++ plugins/wttr.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# The $NAME variable is passed from sketchybar and holds the name of
+# the item invoking this script:
+# https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+LOCATION="Wien"
+TEMP=`curl -s "https://wttr.in/${LOCATION}?format=3" |gsed 's|  *| |g'`
+echo $TEMP
+
+sketchybar --set $NAME label="${TEMP}" \
+	           --set $NAME click_script="/usr/bin/open /System/Applications/Weather.app"
+
